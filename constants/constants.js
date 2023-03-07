249415d5abaa20258bf857664e583c1211bcd3c4
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2023-03-07 15:00:53 +0300

    исправление регулярки

--- constants/constants.js
+++ constants/constants.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 // solt
 export const SOLT_ROUNDS = 10;
 // regex
-export const urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+export const urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
 export const emailRegExp = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 export const limiter = rateLimit({
